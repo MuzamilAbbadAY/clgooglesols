ee3ca7a642855af9ecbfea0fd46d871263b174e6
MuzamilAbbadAY <145654584+MuzamilAbbadAY@users.noreply.github.com>
2026-04-20 18:17:48 +0530

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from typing import Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
-pip install plotly
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
